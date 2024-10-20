f1a4a262c3fad769a64e244b74740d2f05d639d8
Ayalni2289 <alininspor@hotmail.com>
2024-10-21 01:09:32 +0300

    Ödev1 1.aşama tamamlandı. Kullanıcıların girdiği sayılardan çift olanları yazdıran sınıf ve methot çalışıyor.

---
+++ patikaDevÖdev1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Kaç tane sayi girmek istersiniz ? ");
+int a = Convert.ToInt32(Console.ReadLine());
+Odev.Twin(a);
+
+
+public class Odev{
+    public static void Twin(int n){
+        int[] sayilar = new int[n];
+        for(int i=0;i<n;i++){
+             Console.Write("{0}. sayıyı girin: ", i + 1);
+             sayilar[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        foreach (int sayi in sayilar)
+        {
+            if(sayi%2==0){
+                Console.WriteLine("Çift Sayılar: {0} " ,sayi);
+            }
+        }
+    }
+}
