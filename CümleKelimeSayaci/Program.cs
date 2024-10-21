b1e0868254b6585b4240788632cf0a3c216a05ef
Ayalni2289 <alininspor@hotmail.com>
2024-10-22 02:00:09 +0300

    Kelime ve harf sayacımız eklendi çalışıyor.

---
+++ CümleKelimeSayaci/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Bir cümle giriniz");
+string ?cumle = Console.ReadLine();
+
+try
+{
+    Odev.KelimeSayaci(cumle);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hata: " + ex.Message);
+}
+
+
+class Odev{
+    public static void KelimeSayaci(string? a)
+    {
+        if (string.IsNullOrWhiteSpace(a))
+        {
+            Console.WriteLine("Lütfen bir cümle giriniz.");
+            return;
+        }
+
+        int harfSayisi = 0;
+        int kelimeSayisi= a.Split(' ').Length;
+
+        foreach (char item in a)
+        {
+            if(char.IsLetter(item)){
+                harfSayisi++;
+            }
+        }
+        Console.WriteLine("Kelime Sayısı: " + kelimeSayisi);
+        Console.WriteLine("Harf Sayısı: " + harfSayisi);
+    }
+}
