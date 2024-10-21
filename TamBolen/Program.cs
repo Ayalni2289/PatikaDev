551223e5a2decca876d46c70243b97df3dfa149a
Ayalni2289 <alininspor@hotmail.com>
2024-10-21 15:02:46 +0300

    Tam Bölen kodu tamamlandı.

--- TamBolen/Program.cs
+++ TamBolen/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("İki Adet Sayı Giriniz, İlki kaç adet sayı gireceğinizi belirler, İkincisi ise tam bölenini belirler: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+Odev.Tambolen(x,y);
+
+
+class Odev {
+
+    public static void Tambolen(int a, int b){
+        int[] sayilar = new int[a];
+        for(int i=0;i<a;i++){
+             Console.Write("{0}. sayıyı girin: ", i + 1);
+             sayilar[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        foreach (var item in sayilar)
+        {
+            if(item % b ==0){
+                Console.WriteLine("Tam Bölenler: {0} " ,item);
+            }
+        }
+    }
+}
+
